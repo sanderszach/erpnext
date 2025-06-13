7bce3ac7faa1cf63f55edb42de8bb5ca90982d50
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-06-13 14:35:20 +0530

    refactor: get frozen accounts settings from Company in patches

--- erpnext/patches/v14_0/single_to_multi_dunning.py
+++ erpnext/patches/v14_0/single_to_multi_dunning.py
@@ -9,43 +9,47 @@ def execute():
 
 	# Migrate schema of all uncancelled dunnings
 	filters = {"docstatus": ("!=", 2)}
+	for company in frappe.get_all("Company", pluck="name"):
+		filters["company"] = company
+		can_edit_accounts_after = get_accounts_closing_date(company)
+		if can_edit_accounts_after:
+			# Get dunnings after the date when accounts were frozen/closed
+			filters["posting_date"] = (">", can_edit_accounts_after)
 
-	can_edit_accounts_after = get_accounts_closing_date()
-	if can_edit_accounts_after:
-		# Get dunnings after the date when accounts were frozen/closed
-		filters["posting_date"] = (">", can_edit_accounts_after)
+		all_dunnings = frappe.get_all("Dunning", filters=filters, pluck="name")
 
-	all_dunnings = frappe.get_all("Dunning", filters=filters, pluck="name")
+		for dunning_name in all_dunnings:
+			dunning = frappe.get_doc("Dunning", dunning_name)
+			if not dunning.sales_invoice:
+				# nothing we can do
+				continue
 
-	for dunning_name in all_dunnings:
-		dunning = frappe.get_doc("Dunning", dunning_name)
-		if not dunning.sales_invoice:
-			# nothing we can do
-			continue
+			if dunning.overdue_payments:
+				# something's already here, doesn't need patching
+				continue
 
-		if dunning.overdue_payments:
-			# something's already here, doesn't need patching
-			continue
+			payment_schedules = frappe.get_all(
+				"Payment Schedule",
+				filters={"parent": dunning.sales_invoice},
+				fields=[
+					"parent as sales_invoice",
+					"name as payment_schedule",
+					"payment_term",
+					"due_date",
+					"invoice_portion",
+					"payment_amount",
+					# at the time of creating this dunning, the full amount was outstanding
+					"payment_amount as outstanding",
+					"'0' as paid_amount",
+					"discounted_amount",
+				],
+			)
 
-		payment_schedules = frappe.get_all(
-			"Payment Schedule",
-			filters={"parent": dunning.sales_invoice},
-			fields=[
-				"parent as sales_invoice",
-				"name as payment_schedule",
-				"payment_term",
-				"due_date",
-				"invoice_portion",
-				"payment_amount",
-				# at the time of creating this dunning, the full amount was outstanding
-				"payment_amount as outstanding",
-				"'0' as paid_amount",
-				"discounted_amount",
-			],
-		)
+			dunning.extend("overdue_payments", payment_schedules)
+			dunning.validate()
 
-		dunning.extend("overdue_payments", payment_schedules)
-		dunning.validate()
+			dunning.flags.ignore_validate_update_after_submit = True
+			dunning.save()
 
 		dunning.flags.ignore_validate_update_after_submit = True
 		dunning.flags.ignore_links = True
@@ -60,20 +64,21 @@ def execute():
 			make_reverse_gl_entries(voucher_type="Dunning", voucher_no=dunning.name)
 
 
-def get_accounts_closing_date():
+def get_accounts_closing_date(company):
 	"""Get the date when accounts were frozen/closed"""
-	accounts_frozen_till = frappe.db.get_single_value(
-		"Accounts Settings", "acc_frozen_upto"
-	)  # always returns datetime.date
+	accounts_frozen_till_date = frappe.db.get_value("Company", company, "accounts_frozen_till_date")
 
 	period_closing_date = frappe.db.get_value(
-		"Period Closing Voucher", {"docstatus": 1}, "period_end_date", order_by="period_end_date desc"
+		"Period Closing Voucher",
+		{"docstatus": 1, "company": company},
+		"period_end_date",
+		order_by="period_end_date desc",
 	)
 
 	# Set most recent frozen/closing date as filter
-	if accounts_frozen_till and period_closing_date:
-		can_edit_accounts_after = max(accounts_frozen_till, period_closing_date)
+	if accounts_frozen_till_date and period_closing_date:
+		can_edit_accounts_after = max(accounts_frozen_till_date, period_closing_date)
 	else:
-		can_edit_accounts_after = accounts_frozen_till or period_closing_date
+		can_edit_accounts_after = accounts_frozen_till_date or period_closing_date
 
 	return can_edit_accounts_after
