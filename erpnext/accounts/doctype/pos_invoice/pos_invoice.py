0458c548ec82377b7f185b5c3dd9cb29fdb277d8
Diptanil Saha <diptanil@frappe.io>
2025-12-02 17:14:00 +0530

    chore: resolve conflict

--- erpnext/accounts/doctype/pos_invoice/pos_invoice.py
+++ erpnext/accounts/doctype/pos_invoice/pos_invoice.py
@@ -21,11 +21,12 @@ from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
 from erpnext.stock.stock_ledger import is_negative_stock_allowed
 
 
-<<<<<<< HEAD
+
 class PartialPaymentValidationError(frappe.ValidationError):
-=======
+	pass
+
+
 class ProductBundleStockValidationError(frappe.ValidationError):
->>>>>>> 2612152456 (test(pos): add test for product bundle negative stock validation)
 	pass
 
 
