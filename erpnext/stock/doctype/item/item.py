1427b4ac3f6c49feebda25727d8ed8e3c60d3bf2
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-12-04 12:48:07 +0530

    fix: conflicts

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -307,14 +307,8 @@ class Item(Document):
 			if self.stock_ledger_created():
 				frappe.throw(_("Cannot be a fixed asset item as Stock Ledger is created."))
 
-<<<<<<< HEAD
-		if not self.is_fixed_asset:
-			asset = frappe.db.get_all("Asset", filters={"item_code": self.name, "docstatus": 1}, limit=1)
-			if asset:
-=======
 		if not self.is_fixed_asset and not self.is_new():
 			if self.has_submitted_assets():
->>>>>>> 70521fb9bf (fix: remove set_only_once from is_fixed_asset)
 				frappe.throw(
 					_('"Is Fixed Asset" cannot be unchecked, as Asset record exists against the item')
 				)
